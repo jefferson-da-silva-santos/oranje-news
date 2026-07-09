18a8d5b4858c42edee1c87ed868486c298aa7d2b
jefferson-da-silva-santos <jeffrrwpg678@gmail.com>
2026-07-09 02:11:50 -0300

    feat: remove Notyf styles and adjust button width in CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import 'boxicons/css/boxicons.min.css'
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "notyf/notyf.min.css";
 import "./index.css";
 import App from "./App.tsx";
 
