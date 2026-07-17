e60cff3f3d857cdc981a48922f643e1aa74ab664
jefferson-da-silva-santos <jeffrrwpg678@gmail.com>
2026-07-17 02:52:37 -0300

    feat: remove duplicate Boxicons stylesheet import from index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import 'boxicons/css/boxicons.min.css'
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import 'boxicons/css/boxicons.min.css'
 import "./index.css";
 import App from "./App.tsx";
 
